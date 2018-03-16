ae3f28f8e46e303a53aab36b4ad2de9e56a98a85
delianski <dellyan.iliev@gmail.com>
2018-03-16 16:55:26 +0200

    added tui-onui

---
+++ intro-and-basic-syntax/exercise/Phonebook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Phonebook
+{
+	static void Main()
+	{
+		var dict = new Dictionary<string, string>();
+
+		while (true)
+		{
+			var inp = Console.ReadLine().Split(' ');
+
+			switch (inp[0])
+			{
+				case "A":
+					if (dict.ContainsKey(inp[1]))
+					{
+						dict[inp[1]] = inp[2];
+					}
+					else
+					{
+						dict.Add(inp[1],inp[2]);
+					}
+					break;
+				case "S":
+					if (dict.ContainsKey(inp[1]))
+					{
+						Console.WriteLine($"{inp[1]} -> {dict[inp[1]]}");
+					}
+					else
+					{
+						Console.WriteLine($"Contact {inp[1]} does not exist.");
+					}
+					break;
+				case "END":
+					return;
+				default:
+					break;
+			}
+		}
+	}
+}
